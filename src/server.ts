f4795999885a8f410eb8805d2a59da03bb42591f
Kelvin <kelvinnicolau7@gmail.com>
2024-04-02 17:01:31 -0300

    feat: adc prisma e table new

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,26 @@
-interface Pessoa {
-    name: string
-    age: Number
-}
+import fastify from "fastify";
 
-console.log('Hello World')
+const app = fastify()
+
+// Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, ...
+
+// Corpo da requisição (Request Body)
+// Parametros de busca (Search Params / Query Params) 'http://localhost:3333/users?name=Kelvin'
+// Parametros de rota (Route Params) -> Identificação de recursos 'DELETE http://localhost:3333/users/5'
+// Cabeçalhos (Headers) -> Contexto
+
+// Semanticas = Significado
+
+// Driver nativo / Query Builders / ORMs
+
+// Object Relational Mapping (Hibernate / Doctrine / ActiveRecord)
+
+app.post('/events', (request, reply) => {
+    console.log(request.body);
+
+    return 'Hellow NLW United'
+})
+
+app.listen({ port: 3333 }).then(() => {
+    console.log('HTTP SERVER RUNNING!');
+})
