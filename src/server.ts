dab55b1282f82a1085c119faee68d372aa2e3660
Kelvin <kelvinnicolau7@gmail.com>
2024-04-02 17:34:11 -0300

    feat: route events

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import fastify from "fastify";
+import { z } from 'zod';
+import { PrismaClient } from '@prisma/client'
 
 const app = fastify()
+
+const prisma = new PrismaClient({
+    log: ['query'],
+})
 
 // Métodos HTTP: GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS, ...
 
@@ -15,10 +21,27 @@ const app = fastify()
 
 // Object Relational Mapping (Hibernate / Doctrine / ActiveRecord)
 
-app.post('/events', (request, reply) => {
-    console.log(request.body);
+// JSON - JavaScript Object Notation
 
-    return 'Hellow NLW United'
+app.post('/events', async (request, reply) => {
+    const createEventSchema = z.object ({
+        title: z.string(),
+        details: z.string().nullable(),
+        maximumAttendees: z.number().int().positive().nullable(),
+    })
+
+    const data = createEventSchema.parse(request.body);
+
+    const event = await prisma.event.create({
+        data: {
+            title: data.title,
+            details: data.details,
+            maximumAttendees: data.maximumAttendees,
+            slug: new Date().toISOString(),
+        },
+    })
+
+    return reply.status(201).send({ eventId: event.id })
 })
 
 app.listen({ port: 3333 }).then(() => {
